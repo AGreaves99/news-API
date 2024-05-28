a4d1c9a2a0547179d044bbfc4c7da15cd3d0e217
AGreaves99 <ajohngreaves@outlook.com>
2024-05-28 16:53:47 +0100

    finishes happy path for GETting articles by ID

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const express = require("express");
 const app = express();
 const { getTopics } = require("./controllers/topics.controllers");
 const { getApi } = require("./controllers/api.controllers");
+const { getArticleById } = require("./controllers/articles.controllers");
 
 app.get("/api", getApi);
 
 app.get("/api/topics", getTopics);
+
+app.get("/api/articles/:article_id", getArticleById);
+
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else next(err);
+});
 
 app.use((err, req, res, next) => {
   res.status(500).send({ msg: "Internal Server Error" });
