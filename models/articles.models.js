f0772725377f103554dab82f95a6218d96c23e45
AGreaves99 <ajohngreaves@outlook.com>
2024-05-29 11:46:57 +0100

    creates endpoint for GETting articles, sorted by descending date

--- models/articles.models.js
+++ models/articles.models.js
@@ -1,5 +1,18 @@
 const db = require("../db/connection");
 
+exports.selectArticles = () => {
+  return db.query(
+    `SELECT articles.article_id, title, articles.author, topic, articles.created_at, articles.votes, article_img_url, COUNT(comments.comment_id) :: INT comment_count
+    FROM articles
+    LEFT JOIN comments
+    ON articles.article_id = comments.article_id
+    GROUP BY articles.article_id
+    ORDER BY created_at DESC`
+  ).then(({rows}) => {
+    return rows
+  })
+}
+
 exports.selectArticleById = (article_id) => {
   return db.query(
     `SELECT * FROM articles
